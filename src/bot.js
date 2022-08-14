1c7020eb1c205786a226fc27df3953d20e1083e2
FelipeCarlota <felipe.carlota@gmail.com>
2022-08-14 18:04:22 -0300

    configurações

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { token } = require('./commands/tools/config.json');
+const { token } = process.env;
 const { channel } = require("diagnostics_channel");
 const { Client, Collection, GatewayIntentBits,  } = require("discord.js");
 const fs = require("fs");
